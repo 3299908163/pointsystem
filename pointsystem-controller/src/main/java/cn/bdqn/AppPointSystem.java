f96d6c3acd8fa2ce6eec339fd4caf05012b30991
3299908163 <3299908163@qq.com>
2020-11-25 15:38:11 +0800

    提交

---
+++ pointsystem-controller/src/main/java/cn/bdqn/AppPointSystem.java
@@ -0,0 +1,16 @@
+package cn.bdqn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yajun
+ * @create 2019/12/27
+ */
+
+@SpringBootApplication
+public class AppPointSystem {
+    public static void main(String[] args) {
+        SpringApplication.run(AppPointSystem.class,args);
+    }
+}
